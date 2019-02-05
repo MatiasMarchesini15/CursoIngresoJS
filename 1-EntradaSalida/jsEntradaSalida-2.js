d6e73cb95515baa2dd1943ef937e425e2deb4a0e
alumno <alumno>
2019-02-04 22:25:16 -0300

    entrada salida

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -1,7 +1,8 @@
 /*Debemos lograr tomar un nombre con 'prompt' 
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
-{
-	
+{var dato¡ 
+dato=prompt("pata")
+alert(dato)
 }
 
