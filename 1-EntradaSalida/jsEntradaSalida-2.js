277b5b00255ee7d10c7aac7cd18dd1786ee54f0c
MatiasMarchesini15 <matias1512@hotmail.com.ar>
2019-02-05 17:41:00 -0300

    v2

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -1,8 +1,8 @@
 /*Debemos lograr tomar un nombre con 'prompt' 
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
-{var dato¡ 
-dato=prompt("pata")
+{var dato 
+dato=prompt("hola")
 alert(dato)
 }
 
