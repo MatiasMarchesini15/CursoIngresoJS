0d44f9bd04c5c8640aaf861742c312d6c0e90f36
alumno <alumno>
2019-02-05 22:17:19 -0300

    v4

--- 2-InstruccionIf/jsInstruccionIF (3).js
+++ 2-InstruccionIf/jsInstruccionIF (3).js
@@ -1,6 +1,11 @@
 function mostrar()
 {
-//tomo la edad  
-
-
+//tomo la edad
+var edad  
+edad=document.getElementById("edad").value;
+ if (edad >=18) {
+   alert("Tenes más de 18 años");
+}else {
+   alert("Tenes menos de 18 años");
+}
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (4).js
+++ 2-InstruccionIf/jsInstruccionIF (4).js
@@ -1,7 +1,11 @@
 function mostrar()
 {
 //tomo la edad  
-
+var edad
+edad=document.getElementById("edad").Value;
+if(edad>=13 && edad<=17){
+    alert("sos adolecente")
+}
 
 
 }//FIN DE LA FUNCIÓN
