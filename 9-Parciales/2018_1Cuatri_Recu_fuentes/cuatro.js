11e0b3b42c5f419fb91db88f62c6e4a40f9a32e0
MatiasMarchesini15 <matias1512@hotmail.com.ar>
2019-02-26 19:20:50 -0300

    parciales

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
@@ -1,16 +1,20 @@
 function mostrar()
 {
-    var libros=0,tarjeta=0,descuento,total=0, recargo, pf;
-    libros=prompt("Cantidad de libros");
-    libros=parseInt(libros);
-    total=prompt("Precio total");
-    total=parseInt(total);
-    tarjeta=prompt("tarjeta (si/no)");
-    if{ (libros>2)
-    descuento= 
-       
+  var importe,importe2,importe3,importe4,suma;
+  importe=prompt("ingrese importe");
+  importe2=prompt("ingrese importe");
+  importe3=prompt("ingrese importe");
+  importe4=prompt("ingrese importe");
+  importe=parseInt(importe);
+  importe2=parseInt(importe2);
+  importe3=parseInt(importe3);
+  importe4=parseInt(importe4);
+  if (importe > importe2){
+    if (importe > importe3){
+      alert("El primer importe es más grande");
+    }else{
+      alert("El tercer importe es más grande")
+      }
     }
-
-
 
 }
